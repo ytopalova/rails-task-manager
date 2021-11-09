0883c4e6101d53ada52f3ef2b906c24132d5c529
Yuliia Topalova <yuliia.topalova@gmail.com>
2021-11-09 15:19:25 +0000

    done but not styled

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,34 @@
+class TasksController < ApplicationController
+  before_action :find_task, only: [:show, :destroy]
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show; end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+
+  def find_task
+    @task = Task.find(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  Rails.application.routes.draw do
+    get '/tasks', to: 'tasks#index'
+    get '/tasks/new', to: 'tasks#new', as: :new_task
+    post '/tasks', to: 'tasks#create'
+    get '/tasks/:id', to: 'tasks#show', as: :task
+    delete '/tasks/:id', to: 'tasks#destroy'
+  end
 end
